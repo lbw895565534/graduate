1770dc613c271e0faa41dc11f18c26f5b83557dc
cookie.li <cookie.li@onedollartech.com>
2018-03-20 23:15:08 +0800

    update

--- src/pages/share/shareInput.vue
+++ src/pages/share/shareInput.vue
@@ -6,13 +6,15 @@
       </div>
     </div>
     <div class="container_name">
-      <span class="name"></span>
+      <div class="name" type="text"></div>
     </div>
+    <hr>
     <div class="conatiner_info">
-      <div class="info">
-
+      <div class="box_info">
+        <div class="info" contenteditable= "true" placeholder="请输入简介"></div>
       </div>
     </div>
+    <hr>
     <div class="container_step">
       <div class="step">
 
@@ -27,12 +29,53 @@
 
 </script>
 <style scoped>
+  hr {
+    width: 90%;
+    size: 1px;
+    margin: 0 auto;
+  }
   .container_picture {
     width: 100%;
     height: 225px;
+    background: #999;
   }
-  .picture{
-    width: 90%;height: 100%;
-    margin: 10px auto;
+  
+  .container_name {
+    width: 100%;
+    height: 48px;
+  }
+
+  .name {
+    height: 34px;
+    width: 80%;
+    line-height: 48px; 
+    border: 1px solid #999;
+    margin: 0 auto;
+    position: relative;
+    top: 6px;
+  }
+
+  .conatiner_info{
+    width: 100%;
+  }
+  .box_info {
+    width: 90%;
+    height: auto;
+    margin: 0 auto;
+  }
+  .info {
+    width: 100%;
+    min-height: 30px;
+    max-height: 245px;
+    line-height: 30px;
+    font-size: 20px;
+    color: #999;
+    overflow: scroll;
+    overflow-x: hidden;
+    outline: none;
+  }
+  .conatiner_step{
+    width: 100%;
+    height: 
   }
 </style>
