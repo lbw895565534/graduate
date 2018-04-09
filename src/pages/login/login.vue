052d13ac530e8920dcb7b175262ba94b2922f29c
cookie.li <cookie.li@onedollartech.com>
2018-04-10 01:17:53 +0800

    update

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -157,12 +157,13 @@
     height: 100%;
     display: inline-block;
     float: left;
+    position: relative;
   }
 
   .icon {
     width: 20px;
     height: 20px;
-    position: relative;
+    position: absolute;
     top: 50%;
     left: 50%;
     margin-top: -10px;
